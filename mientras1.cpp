cde4477ee64738a525dbe2443caf76e835da72a1
AronQuito <acachicatariq@unjbg.edu.pe>
2024-06-17 19:01:14 -0500

    Actualiza mientras ejercicio 1 de estructuras repetitivas: agrega validacion de cero en entrada junto a una visual

--- mientras1.cpp
+++ mientras1.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 int main(){
     int edad,suma=0,nAlum,i=1;
-    cout<<"Cantidad de alumnos: "; cin>>nAlum;
+    system("cls");
+    cout<<"Sumatoria de edades de alumnos!"<<endl<<endl;
+    cout<<"Introduce la cantidad de alumnos: "; cin>>nAlum;
     while(i<=nAlum){
-        cout<<"Introduce la edad: "; cin>>edad;
-        suma+=edad;
-        i++;
+        system("cls");
+        cout<<"Introduce la edad del alumno "<<i<<" : "; cin>>edad;
+        if(edad<=0){
+            cout<<endl<<"Introduce una edad valida para la sumatoria"<<endl;
+            system("pause");
+        }else{
+            suma+=edad;
+            i++;
+        }
     }
-    cout<<"Sumatoria:"<<suma;
+    system("cls");
+    cout<<"La sumatoria de las edades es: "<<suma;
     return 0;
 }
